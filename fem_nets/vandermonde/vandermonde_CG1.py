8af307c937d1ba2a970ea86ee24217f452e7560d
mirok <miroslav.kuchta@gmail.com>
2023-02-09 14:09:12 +0100

    update docs

--- fem_nets/vandermonde/vandermonde_CG1.py
+++ fem_nets/vandermonde/vandermonde_CG1.py
@@ -4,7 +4,7 @@ import torch
 
 
 def compute_vandermonde_CG1(x, mesh, tol_=1E-13):
-
+    '''Vandermonde matrix has in colums values of basis functions at x'''
     batchdim, npts, gdim = x.shape
     assert gdim == 2
     assert batchdim == 1
@@ -18,13 +18,18 @@ def compute_vandermonde_CG1(x, mesh, tol_=1E-13):
     dm = V.dofmap()
 
     ndofs = V.dim()
-    
+    # The idea for evaluation is similar to how fenics does interpolation.
+    # However, we do not find first the cell colliding with points, as we 
+    # evaluate batches. Instead for everycell we decide based on barycentric
+    # coordinates where which elements in `x` are inside. Based on the cooerds
+    # we also compute the basis function value
     out = torch.zeros(npts, dtype=dtype)
-
+    # The things with CG space and inside check (within tol) is that some points
+    # migth end up found in several cells resulting in duplicate contributions.
+    # We try to make exach xi contribute only once below
     x_has_contributed = torch.zeros(npts, dtype=bool)    
     
     vandermonde = torch.zeros(batchdim, npts, ndofs, dtype=dtype)
-
     for cell in df.cells(mesh):
         cell_dofs = dm.cell_dofs(cell.index())
         
@@ -60,15 +65,3 @@ def compute_vandermonde_CG1(x, mesh, tol_=1E-13):
             out *= 0
         x_has_contributed[is_inside] = True
     return vandermonde
-    
-# --------------------------------------------------------------------
-
-if __name__ == '__main__':
-    from utils import cell_centers
-    
-    mesh = df.UnitSquareMesh(1, 1)
-
-    x_ = cell_centers(mesh)
-    x = torch.tensor(x_, dtype=torch.float64).reshape((1, ) + x_.shape)
-
-    V = compute_vandermonde(x, mesh)
